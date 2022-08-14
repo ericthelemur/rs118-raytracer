307380cda2db02c8d6e5a55e89c1d13efde22316
ericthelemur <owenconnors1@gmail.com>
2022-08-14 13:35:21 +0100

    Task 1.2: Gradients

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use image::{RgbImage, Rgb};
 
+fn f2b(v: f32) -> u8 {
+    return (v * 256.0) as u8;
+}
+
 fn main() {
-    let mut img = RgbImage::from_fn(256, 256, |x, y| {
-        Rgb([255, 0, 0])
+    let (w, h) = (256, 256);
+    let mut img = RgbImage::from_fn(w, h, |x, y| {
+        let i = (x as f32) / (w as f32);
+        let j = (y as f32) / (h as f32);
+        Rgb([i, j, 0.25].map(f2b))
     });
     img.save("test.png").expect("Eror writing image");
 }
