373af3162f859b722032c527590ed6ee146c76ca
ericthelemur <owenconnors1@gmail.com>
2022-08-14 13:28:55 +0100

    Task 1.1: Red

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use image::{RgbImage, Rgb};
+
 fn main() {
-    println!("Hello, world!");
+    let mut img = RgbImage::from_fn(256, 256, |x, y| {
+        Rgb([255, 0, 0])
+    });
+    img.save("test.png").expect("Eror writing image");
 }
