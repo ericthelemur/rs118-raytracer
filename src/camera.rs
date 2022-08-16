0db8a3e8904426d47ddcf7afbc35e690296aa319
ericthelemur <owenconnors1@gmail.com>
2022-08-16 23:57:56 +0100

    Refactor: camera axes and px outside

--- src/camera.rs
+++ src/camera.rs
@@ -1,34 +1,41 @@
+use derive_more::Constructor;
+
 use crate::ray::Ray;
 use crate::vector::{Vec3, Point};
 use crate::v;
 
+#[derive(Debug, Constructor)]
+pub struct Axes {
+    pub x: Vec3,
+    pub y: Vec3,
+    pub z: Vec3,
+}
+
+
+
 #[derive(Debug)]
 pub struct Camera {
-    pub vw: f64,
-    pub vh: f64,
-    pub pxw: u32,
-    pub pxh: u32,
-    pub f: f64,
+    pub pos: Point,
+    pub axes: Axes,
 }
 
 impl Camera {
-    pub fn new(px_w: u32, fov: f64, aspect_ratio: f64) -> Self {
+    pub fn new(fov: f64, aspect_ratio: f64) -> Self {
         let angle = fov.to_radians();
         let vh = (angle / 2.).tan() * 2.;
 
-        dbg!(Self { pxw: px_w, pxh: ((px_w as f64) / aspect_ratio) as u32, vw: vh * aspect_ratio, vh: vh, f: 1.0 })
+        dbg!(Self { 
+            pos: v!(),
+            axes: Axes::new(v!(vh * aspect_ratio, 0, 0), v!(0, vh, 0), v!(0, 0, 1)),
+        })
     }
 
     pub fn tl(&self) -> Vec3 {
-        v!(-self.vw/2.0, self.vh/2.0, -self.f)
-    }
-    
-    pub fn br(&self) -> Vec3 {
-        v!(self.vw/2.0, -self.vh/2.0, -self.f)
+        self.pos - self.axes.x/2. + self.axes.y/2. - self.axes.z
     }
 
     pub fn get_ray(&self, x: f64, y: f64) -> Ray {
-        let vxy = v!(x, y, 0).rescale(v!(), v!(self.pxw, self.pxh, 0), self.tl(), self.br());
+        let vxy = self.tl() + x * self.axes.x - y * self.axes.y;
         Ray::towards(v!(), vxy)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod material;
 use std::f64::consts::PI;
 
 use camera::Camera;
+use derive_more::Constructor;
 use image::{RgbImage};
 use indicatif::ParallelProgressIterator;
 use lerp::Lerp;
@@ -32,7 +33,10 @@ fn colour(ray: &Ray, scene: &Scene, depth: u32) -> Colour {
 fn main() {
     let samples = 50;
     let max_depth = 20;
-    let c = Camera::new(400, 90., 16. / 9.);
+    let asp_ratio = 16. / 9.;
+    let pxw = 400;
+    let pxh = ((pxw as f64) / asp_ratio) as u32;
+    let c = Camera::new(90., asp_ratio);
 
     let R = (PI/4.).cos();
     let scene: Scene = vec![
@@ -47,7 +51,7 @@ fn main() {
             Lambertian::new(v!(1, 0, 0)),
         )),
     ];
-    let bar = indicatif::ProgressBar::new((c.pxw * c.pxh) as u64);
+    let bar = indicatif::ProgressBar::new((pxw * pxh) as u64);
     bar.set_style(
         indicatif::ProgressStyle::default_bar()
             .template(
@@ -58,7 +62,7 @@ fn main() {
     );
     bar.set_draw_rate(5);
 
-    let mut img = RgbImage::new(c.pxw, c.pxh);
+    let mut img = RgbImage::new(pxw, pxh);
     img.enumerate_pixels_mut()
         .par_bridge()
         .progress_with(bar)
@@ -67,7 +71,8 @@ fn main() {
             let colour = (0..samples).map(|_| {
                     let (rx, ry): (f64, f64) = (rng.gen(), rng.gen());
                     let (sx, sy) = (x as f64 - 0.5 + rx, y as f64 - 0.5 + ry);
-                    let ray = c.get_ray(sx, sy);
+                    let (ux, uy) = (sx / pxw as f64, (pxh as f64 - sy) / pxh as f64);
+                    let ray = c.get_ray(ux, uy);
                     colour(&ray, &scene, max_depth)
                 }).fold(v!(), |acc, x| acc + x) / (samples as f64);
             *p = colour.into()
